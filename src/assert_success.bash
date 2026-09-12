e4ce3fa63181164ee42edaff51c32580502ece16
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-09-12 16:18:01 +0200

    lint: lint .bash files

--- src/assert_success.bash
+++ src/assert_success.bash
@@ -33,15 +33,16 @@ assert_success() {
   : "${output?}"
   : "${status?}"
 
-  if (( status != 0 )); then
-    { local -ir width=6
+  if ((status != 0)); then
+    {
+      local -ir width=6
       batslib_print_kv_single "$width" 'status' "$status"
       batslib_print_kv_single_or_multi "$width" 'output' "$output"
       if [[ -n "${stderr-}" ]]; then
-      	batslib_print_kv_single_or_multi "$width" 'stderr' "$stderr"
+        batslib_print_kv_single_or_multi "$width" 'stderr' "$stderr"
       fi
-    } \
-    | batslib_decorate 'command failed' \
-    | fail
+    } |
+      batslib_decorate 'command failed' |
+      fail
   fi
 }
